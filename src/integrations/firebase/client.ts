5bbd7d2c49627221a9466f23361f0ae391f8a9d6
primetoysrajkot <primetoysrajkot@gmail.com>
2026-01-06 18:43:49 +0000

    feat: Add row-level security for stocks and automatic timestamp updates

---
+++ src/integrations/firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXj_yGFjW5kOlQI1M7VF-SSzi_xNhR9VE",
+  authDomain: "prime-toys-stock-manage-app.firebaseapp.com",
+  projectId: "prime-toys-stock-manage-app",
+  storageBucket: "prime-toys-stock-manage-app.firebasestorage.app",
+  messagingSenderId: "277274959050",
+  appId: "1:277274959050:web:47aedf20bf24d21e262833"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
